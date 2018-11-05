d4b2c78e0b6646dc2f9855991d0f812310a4c877
Alec Feldman <afeldman1212@students.mc3.edu>
2018-11-05 11:53:43 -0500

    Changed to maven project.

--- VideoSync/src/VideoSync.java
+++ VideoSync/src/VideoSync.java
@@ -4,6 +4,16 @@ public class VideoSync
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello from desktop!");
+		Configuration config = new Configuration();
+		config.setHostname("localhost");
+		config.setPort(1337);
+		config.setPingTimeout(30);
+		
+		SocketConfig socketConfig = new SocketConfig();
+		socketConfig.setReuseAddress(true);
+		
+		config.setSocketConfig(socketConfig);
+		
+		SocketIOServer server = new SocketIOServer(config);
 	}
 }
