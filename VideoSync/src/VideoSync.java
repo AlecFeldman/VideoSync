dec8f11b78da25352d229b5c9581154ef465132a
Alec Feldman <afeldman1212@students.mc3.edu>
2018-11-07 00:37:48 -0500

    Using TomP2P.

--- VideoSync/src/VideoSync.java
+++ VideoSync/src/VideoSync.java
@@ -1,35 +1,30 @@
-import java.io.UnsupportedEncodingException;
-
-import com.corundumstudio.socketio.AckRequest;
-import com.corundumstudio.socketio.Configuration;
-import com.corundumstudio.socketio.SocketIOClient;
-import com.corundumstudio.socketio.SocketIOServer;
-import com.corundumstudio.socketio.Transport;
-import com.corundumstudio.socketio.listener.DataListener;
+import java.io.IOException;
 
 public class VideoSync
 {
 	public static void main(String[] args) throws InterruptedException
 	{
-		Configuration config = new Configuration();
-        config.setHostname("localhost");
-        config.setPort(9092);
-        config.setMaxFramePayloadLength(1024 * 1024);
-        config.setMaxHttpContentLength(1024 * 1024);
-
-        final SocketIOServer server = new SocketIOServer(config);
-
-        server.addEventListener("msg", byte[].class, new DataListener<byte[]>() {
-            @Override
-            public void onData(SocketIOClient client, byte[] data, AckRequest ackRequest) {
-                client.sendEvent("msg", data);
-            }
-        });
-
-        server.start();
-
-        Thread.sleep(Integer.MAX_VALUE);
-
-        server.stop();
+		class MessageListenerImpl implements MessageListener{
+			int peerid;
+			public MessageListenerImpl(int peerid)
+			{
+				this.peerid=peerid;
+			}
+			public Object parseMessage(Object obj) {
+				System.out.println(peerid+"] (Direct Message Received) "+obj);
+				return "success";
+			}
+			
+		}
+		
+		try {
+			PublishSubscribeImpl peer0 = new PublishSubscribeImpl(0, "127.0.0.1", new MessageListenerImpl(0));
+			
+			peer0.createTopic("Video");
+			peer0.subscribetoTopic("Video");
+			peer0.publishToTopic("Video", "Hello from peer0!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
