f697ae7dca4a8c8ed615effc7115851b1f72fd1c
Alec Feldman <afeldman1212@students.mc3.edu>
2018-11-12 02:22:12 -0500

    Trying something new.

--- VideoSync/src/VideoSync.java
+++ VideoSync/src/VideoSync.java
@@ -1,130 +1,101 @@
 import java.io.IOException;
-import java.net.Inet6Address;
 import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Scanner;
+
+import net.tomp2p.dht.FutureGet;
+import net.tomp2p.dht.PeerBuilderDHT;
+import net.tomp2p.dht.PeerDHT;
+import net.tomp2p.futures.FutureBootstrap;
+import net.tomp2p.futures.FutureDirect;
+import net.tomp2p.futures.FutureDiscover;
+import net.tomp2p.p2p.PeerBuilder;
+import net.tomp2p.peers.Number160;
+import net.tomp2p.peers.PeerAddress;
+import net.tomp2p.rpc.ObjectDataReply;
+import net.tomp2p.storage.Data;
 
 public class VideoSync
 {
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws IOException
 	{
 		int option;
 		
-		String ip = "";
-		String topicName = "";
+		String _topic_name = "Videos";
+		String _obj = "Hello from laptop!";
 		
 		Scanner keys = new Scanner(System.in);
+		
+		HashSet<PeerAddress> peers_on_topic = new HashSet<>();
 		
 		System.out.print("1. Create server\n2. Join server\nEnter option: ");
 		option = keys.nextInt();
 		
-		keys.nextLine();
+		PeerDHT peer = new PeerBuilderDHT(new PeerBuilder(Number160.createHash(0)).ports(4000).start()).start();
+		
+		FutureBootstrap fb = peer.peer().bootstrap().inetAddress(InetAddress.getByName("127.0.0.1")).ports(4000).start();
+		fb.awaitUninterruptibly();
+		if(fb.isSuccess())
+		{
+			peer.peer().discover().peerAddress(peer.peerAddress()).start().awaitUninterruptibly();
+		}
+		
+		peer.peer().objectDataReply(new ObjectDataReply()
+		{
+			public Object reply(PeerAddress sender, Object request) throws Exception
+			{
+				System.out.println(request);
+				return "success";
+			}
+		});
+		
+		FutureGet futureGet;
 		
 		if (option == 1)
 		{
-			ip = getIPv4Address();
-			
-			System.out.print("Create a topic: ");
-			topicName = keys.nextLine();
-		}
-		else if (option == 2)
-		{
-			System.out.print("Enter IP address: ");
-			ip = keys.nextLine();
-			
-			System.out.print("Enter a topic: ");
-			topicName = keys.nextLine();
-		}
-		
-		joinServer(option, ip, topicName, keys);
-		keys.close();
-		System.exit(0);
-	}
-	
-	public static void joinServer(int option, String ip, String topicName, Scanner keys) throws IOException
-	{
-		boolean quitServer = false;
-		
-		String message = "";
-		
-		try
-		{
-			// ID must be different for each client.
-			PublishSubscribeImpl peer = new PublishSubscribeImpl(1, ip, new MessageListenerImpl(1));
-			
-			if (option == 1)
-				peer.createTopic(topicName);
-			
-			peer.subscribetoTopic(topicName);
-			
-			while (!quitServer)
+			System.out.println("Creating topic...");
+			futureGet = peer.get(Number160.createHash(_topic_name)).start();
+			futureGet.awaitUninterruptibly();
+			if (futureGet.isSuccess() && futureGet.isEmpty())
 			{
-				System.out.print("> ");
-				message = keys.nextLine();
-				
-				if (message.startsWith("/"))
-				{
-					// More server commands will be added here later.
-					switch(message.substring(1))
-					{
-						case "quit":
-							quitServer = true;
-							break;
-						default:
-							System.out.println("Different commands will be printed here.");
-					}
-				}
-				else
-				{
-					peer.publishToTopic(topicName, message);
-				}
-			}
-			
-			System.out.println("Quiting server...");
-			peer.unsubscribeFromTopic(topicName);
-			peer.leaveNetwork();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	public static String getIPv4Address()
-	{
-		String ip = "";
-		
-		try
-		{
-			Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-			
-			while (interfaces.hasMoreElements())
-			{
-				NetworkInterface iface = interfaces.nextElement();
-				
-				if (iface.isLoopback() || !iface.isUp())
-					continue;
-				
-				Enumeration<InetAddress> addresses = iface.getInetAddresses();
-				
-				while(addresses.hasMoreElements())
-				{
-					InetAddress addr = addresses.nextElement();
-					
-					if (addr instanceof Inet6Address)
-						continue;
-					
-					ip = addr.getHostAddress();
-				}
+				peer.put(Number160.createHash(_topic_name)).data(new Data(new HashSet<PeerAddress>())).start().awaitUninterruptibly();
 			}
 		}
-		catch (SocketException e)
+		
+		futureGet = peer.get(Number160.createHash(_topic_name)).start();
+		futureGet.awaitUninterruptibly();
+		if (futureGet.isSuccess())
 		{
-			throw new RuntimeException(e);
+			try
+			{
+				peers_on_topic = (HashSet<PeerAddress>) futureGet.dataMap().values().iterator().next().object();
+				peers_on_topic.add(peer.peer().peerAddress());
+				peer.put(Number160.createHash(_topic_name)).data(new Data(peers_on_topic)).start().awaitUninterruptibly();
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
 		}
 		
-		return ip;
+		futureGet = peer.get(Number160.createHash(_topic_name)).start();
+		futureGet.awaitUninterruptibly();
+		if (futureGet.isSuccess())
+		{
+			try
+			{
+				peers_on_topic = (HashSet<PeerAddress>) futureGet.dataMap().values().iterator().next().object();
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+			for(PeerAddress p : peers_on_topic)
+			{
+				FutureDirect futureDirect = peer.peer().sendDirect(p).object(_obj).start();
+				futureDirect.awaitUninterruptibly();
+			}
+		}
 	}
 }
