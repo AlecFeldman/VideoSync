55a4030accc5be7beac0594e3fa533eefdba2172
Alec Feldman <afeldman1212@students.mc3.edu>
2018-12-12 22:28:58 -0500

    Add missing packet data.

--- VideoSync/src/SerializedPacket.java
+++ VideoSync/src/SerializedPacket.java
@@ -2,9 +2,10 @@ import java.io.Serializable;
 
 import io.humble.ferry.Buffer;
 import io.humble.video.MediaPacket;
+import io.humble.video.Rational;
 
 public class SerializedPacket implements Serializable
-{
+{	
 	private byte[] rawData;
 	
 	private int index;
@@ -16,6 +17,11 @@ public class SerializedPacket implements Serializable
 	private long duration;
 	private long convergenceDuration;
 	private long position;
+	private long timeStamp;
+	
+	private boolean isKey;
+	
+	private Rational timeBase;
 	
 	public SerializedPacket(MediaPacket packet)
 	{
@@ -27,6 +33,9 @@ public class SerializedPacket implements Serializable
 		duration = packet.getDuration();
 		convergenceDuration = packet.getConvergenceDuration();
 		position = packet.getPosition();
+		timeStamp = packet.getTimeStamp();
+		isKey = packet.isKeyPacket();
+		timeBase = packet.getTimeBase();
 	}
 
 	public MediaPacket getPacket()
@@ -40,6 +49,9 @@ public class SerializedPacket implements Serializable
 		packet.setDuration(duration);
 		packet.setConvergenceDuration(convergenceDuration);
 		packet.setPosition(position);
+		packet.setTimeStamp(timeStamp);
+		packet.setKeyPacket(isKey);
+		packet.setTimeBase(timeBase);
 		
 		return packet;
 	}
